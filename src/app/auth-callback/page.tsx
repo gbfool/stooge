245941b4f4bbe9c878dead2acb35191bff414d30
humblefoool <humbledevs@gmail.com>
2023-10-21 12:16:25 +0530

    finished working on database sync

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,38 @@
+"use client"
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import React from "react";
+import { Loader2 } from "lucide-react";
 
 const Page = () => {
   const router = useRouter();
-  const searchParams = useSearchParams()
-  const origin = searchParams.get('origin')
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
+
+  React.useEffect(() => {
+    if (data?.success) {
+      router.push(origin ? `/${origin}` : "/dashboard");
+    }
+
+    if (error?.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
+    }
+  }, [data, error, origin, router]);
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirecting automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
